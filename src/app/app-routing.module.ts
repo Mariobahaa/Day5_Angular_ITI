35c76deea04823d9354539f89a5121ecd1ce4e01
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2021-03-18 20:04:59 +0200

    Init Routing Module + Table Structure + link click

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {EmployeesComponent} from "./components/employees/employees.component"
+import {EmpdetailsComponent} from "./components/empdetails/empdetails.component"
+import { ErrorComponent } from './components/error/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "employees",  pathMatch:"full"},
+  {path: "employees", component: EmployeesComponent,  pathMatch:"full" },
+  {path: "employees/:id", component: EmpdetailsComponent,  pathMatch:"full"},
+  {path: "**", component: ErrorComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeesComponent } from './components/employees/employees.component';
+import { EmpdetailsComponent } from './components/empdetails/empdetails.component';
+import { ErrorComponent } from './components/error/error.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesComponent,
+    EmpdetailsComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
